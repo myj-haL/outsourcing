1e6cc4bbed158962269ac08166325021dc4a137d
myj <myj262@gmail.com>
2025-07-17 23:19:44 +0900

    강사지원

--- work/beyondClass/assets/js/loader.js
+++ work/beyondClass/assets/js/loader.js
@@ -1,6 +1,6 @@
 function loadLayoutComponents() {
   const currentPath = window.location.pathname.split('/').filter(Boolean).pop();
-  const editUrl = (currentPath === 'contact' || currentPath === 'recruit' || currentPath === 'about' || currentPath === 'serviceIntroduce')  ? "../" : "./";
+  const editUrl = (currentPath === 'about' || currentPath === 'apply')  ? "../" : "./";
 
   $('#header').load(editUrl + 'theme/header.html', function() {
     // activateMenu();
